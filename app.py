360297cf726098355a40deef81acf94291999466
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-03-20 03:40:37 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# Creating a Web App From Scratch Using Python Flask and MySQL
+# https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
+
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
