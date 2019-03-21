a8e89b932d310b42eba0010b8f72138d7859d995
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-03-20 23:30:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
