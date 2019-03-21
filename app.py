7ef94b919b6cad5d5b42307133cf3ac25edcfee7
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-03-21 02:44:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,18 @@
 # https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
 
 from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
  
 app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL Configurations
+app.config['MYSQL_DATABASE_USER'] = 'UsernameGoesHERE'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'PasswordGoesHERE'
+app.config['MYSQL_DATABASE_DB'] = 'bucket_list_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost' 
+mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -15,13 +25,28 @@ def showSignUp():
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
-    # Get Form Data
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+    try:
+	    # Get Form Data
+	    _name = request.form['inputName']
+	    _email = request.form['inputEmail']
+	    _password = request.form['inputPassword']
 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+	    # validate the received values
+	    if _name and _email and _password:
+	        conn = mysql.connect()
+	        cursor = conn.cursor()
+	        _hashed_password = generate_password_hash(_password)
+	        cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+	        data = cursor.fetchall()
+	        if len(data) is 0:
+	            conn.commit()
+	            return json.dumps({'message': 'User created successfully!'})
+	        else:
+	            return json.dumps({'error': str(data[0])})
+	    else:
+	        return json.dumps({'html':'<span>Enter the required fields</span>'})
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
