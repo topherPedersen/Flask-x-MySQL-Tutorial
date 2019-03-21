38c162d883a69f97f141fd60521578683f435ff8
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-03-21 01:53:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Creating a Web App From Scratch Using Python Flask and MySQL
 # https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
  
 app = Flask(__name__)
 
@@ -12,3 +12,16 @@ def index():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    # Get Form Data
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
